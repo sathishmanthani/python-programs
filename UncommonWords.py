d83674abb18318ec9ef16d6c3ac16945eabb00b9
Sathish Manthani <43104471+sathishmanthani@users.noreply.github.com>
2020-09-30 21:30:56 -0700

    Create UncommonWords.py

---
+++ UncommonWords.py
@@ -0,0 +1,13 @@
+s1 = 'Python is an interpreted, high-level and general-purpose programming language'
+s2 = 'Python is Created by Guido van Rossum and first released in 1991'
+
+print(set(s1.split()).symmetric_difference(s2.split()))
+
+# Remember that Symmetric_difference is a function of a Set and accepts any iterable. So its not necessary to convert the second list to Set.
+
+# you can also write it like below
+a = set(s1.split())
+b = set(s2.split())
+
+print((a-b).union(b-a))
+
