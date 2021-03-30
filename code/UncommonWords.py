23ac4c9e229f9f2d8c62aedb445806c4f36140f9
Sathish Manthani <43104471+sathishmanthani@users.noreply.github.com>
2021-03-29 21:54:34 -0700

    Update UncommonWords.py

--- code/UncommonWords.py
+++ code/UncommonWords.py
@@ -2,8 +2,15 @@
 s1 = 'Python is an interpreted, high-level and general-purpose programming language'
 s2 = 'Python is Created by Guido van Rossum and first released in 1991'
 
-print(set(s1.split()).symmetric_difference(s2.split()))
-
+if s1 and s2:
+    print(set(s1.split()).symmetric_difference(s2.split()))
+elif s1 and not s2:
+    print(s1.split())
+elif not s1 and s2:
+    print(s2.split())
+else:
+    print('empty')
+    
 # Remember that Symmetric_difference is a function of a Set and accepts any iterable. So its not necessary to convert the second list to Set.
 
 # you can also write it like below
